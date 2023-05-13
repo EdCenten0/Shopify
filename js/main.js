92067e6acbc647bda3c5504ccf05421e2cb724cc
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-05-13 15:43:23 -0600

    added mobile-menu event

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 const navbar_email = document.querySelector('.navbar-email')
 const desktop_menu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-navbar_email.addEventListener('click', toggleDesktopMenu)
-
+navbar_email.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktop_menu.classList.toggle('inactive')
 }
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
+  }
 
